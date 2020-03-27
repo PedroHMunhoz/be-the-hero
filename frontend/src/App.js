03b484d02148711bcfa556867c036f857ac1fa21
PedroHMunhoz <pedromunhozc@gmail.com>
2020-03-27 01:07:26 -0300

    Front-end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,29 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
+
+import "./global.css";
+
+// import Header from "./Header";
+// import Logon from "./pages/Logon";
+import Routes from "./routes";
+
+//Um componente no React é uma function que retorna HTML
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  //UseState retorna um array de 2 posições [valor, funcaoDeAtualizacao]
+
+  // function increment() {
+  //   setCounter(counter + 1);
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello OmniStack</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes />
+      {/* <Header>Contador: {counter}</Header>
+      <button onClick={increment}>Incrementar</button> */}
     </div>
   );
+  // return <h1>Hello World</h1>;
 }
 
 export default App;
